0e3dac077fb467ce0c07ec9b99e0ef743e649b7d
Brian <brian.qiu9522@hotmail.com>
2025-03-13 14:49:33 -0400

    button to navigate to create event page

--- frontend/lib/clubs/club_home_page.dart
+++ frontend/lib/clubs/club_home_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 import 'club.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
+import 'create_event.dart';
 
 class ClubHomePage extends StatelessWidget {
   final Club club;
 
   ClubHomePage({required this.club});
+
+  void _createEvent(BuildContext context) async {
+    PersistentNavBarNavigator.pushNewScreenWithRouteSettings(
+      context,
+      settings: RouteSettings(name: "CreateEventScreen"),
+      screen: CreateEventPage(),
+      withNavBar: true,
+      pageTransitionAnimation: PageTransitionAnimation.cupertino,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +43,11 @@ class ClubHomePage extends StatelessWidget {
               "Description: ${club.description}",
               style: TextStyle(fontSize: 16),
             ),
-            // Add more details or functionality as needed
+            SizedBox(width: 10),
+            ElevatedButton(
+              onPressed: () => _createEvent(context), // Pass context here
+              child: Text('Create Event'),
+            ),
           ],
         ),
       ),
